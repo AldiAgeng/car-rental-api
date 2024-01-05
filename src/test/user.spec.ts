9e4a0995825715a81bf0297df8273f8fbeb93865
AldiAgeng <aldiageng48@gmail.com>
2024-01-05 20:15:20 +0700

    log user spec

--- src/test/user.spec.ts
+++ src/test/user.spec.ts
@@ -52,6 +52,7 @@ describe('[POST] register', () => {
       email: 'testuser2@yopmail.com',
       password: await hashPassword('testuser2')
     })
+    console.log('hasil', result.body.message)
     expect(result.status).toBe(201)
     expect(result.body.message).toBe('Data disimpan')
   })
@@ -71,6 +72,7 @@ describe('[POST] register', () => {
       email: 'testuser2@yopmail.com',
       password: await hashPassword('testuser2')
     })
+    console.log('hasil', result.body.message)
     expect(result.status).toBe(401)
     expect(result.body.message).toBe('Email already exist')
   })
