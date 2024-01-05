4da08eecf7b5da4a2a55cb0a505bae72ea5f1603
AldiAgeng <aldiageng48@gmail.com>
2024-01-05 20:11:52 +0700

    ci:

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -4,9 +4,9 @@ import { config } from 'dotenv'
 config()
 
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME ?? 'dewpe0hc8',
+  api_key: process.env.CLOUDINARY_API_KEY ?? '826876771189152',
+  api_secret: process.env.CLOUDINARY_API_SECRET ?? '7UtsjzeqoMHes3YBqWdXpRl-VWc',
   secure: true
 })
 
